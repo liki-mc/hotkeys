fc9c9a9a935ea86505371ff47df1061e6ad2565a
liki-mc <liki.mc@outlook.com>
2025-01-11 13:06:14 +0100

    added listbox to application

--- src/main.py
+++ src/main.py
@@ -1,15 +1,39 @@
 import tkinter as tk
 import keyboard
 
+from .scrollable_frame import ListBox
+
 class App:
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title("My Application")
-        self.root.geometry("900x600")  # Set the size of the window
+        self.root.title("Hotkeys")
+        self.root.geometry("900x600")
         self.root.after(50, self.check)
 
         # Bind the close event to hide the window
         self.root.protocol("WM_DELETE_WINDOW", self.hide_window)
+
+        
+        self.listbox = ListBox(self.root)
+
+        self.listbox.create("Item 10", "This is the tenth item.", tags = ["tenth", "model"])
+        self.listbox.create("Item 1", "This is the first item.", tags = ["first", "example"])
+        self.listbox.create("Itemdqsfqsdfqsdfqs 2", "This is the second item.", tags = ["second", "sample"])
+        self.listbox.create("Item 3", "This is the third item.", tags = ["third", "demo"])
+        self.listbox.create("Item 4", "This is the fourth item.", tags = ["fourth", "test"])
+        self.listbox.create("Item 5", "This is the fifth item.", tags = ["fifth", "trial"])
+        self.listbox.create("Item 6", "This is the sixth item.", tags = ["sixth", "experiment"])
+        self.listbox.create("Item 7", "This is the seventh item.", tags = ["seventh", "prototype"])
+        self.listbox.create("Item 8", "This is the eighth item.", tags = ["eighth", "pilot"])
+        self.listbox.create("Item 9", "This is the ninth item.", tags = ["ninth", "mockup"])
+
+        # Entry widget for filtering
+        self.search_var = tk.StringVar()
+        self.search_entry = tk.Entry(self.root, textvariable = self.search_var)
+        self.search_entry.pack(pady = 10, padx = 10, fill = tk.X)
+        self.listbox.pack(fill = tk.BOTH, expand = True)
+
+        self.search_var.trace("w", lambda name, index, mode, sv = self.search_var: self.listbox.filter(sv.get()))
 
         # Bind the Esc key to hide the window
         self.root.bind('<Escape>', lambda e: self.hide_window())
@@ -19,6 +43,7 @@ class App:
 
     def show_window(self):
         self.root.deiconify()  # Show the window
+        self.search_entry.focus_set()
 
     def hide_window(self):
         self.root.withdraw()  # Hide the window
